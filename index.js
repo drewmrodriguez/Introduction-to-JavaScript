e6da5c6265895da0ab5d60b92f4fab6e89d806c5
drewmrodriguez <drewmrodriguez@gmail.com>
2020-07-25 10:32:46 -0700

    Task, a,c,d, Task 2

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 /************************************************************** Task 1: Warm-up! **************************************************************/
 //Task a: declare a variable called votingAge, console log true if age > 18 (no function required)
 
+let votingAge = 18;
 
-
-
+if (votingAge >= 18) {
+    console.log("true")
+}
 
 //Task b: declare a variable and then use a conditional to change the value of that variable based on the value assigned to a second variable (no function required)
 
@@ -11,25 +13,36 @@
 
 
 
+
 //Task c: Convert string ("1999") to integer (1999)  (no function required) // hint look up the Number method
 
+const year = '1999';
 
+Number(1999);
 
+console.log(typeof(1999));
 
 
 //Task d: Write a function to multiply a*b 
 
+function multiply () {
+    let result = a*b;
+    return (a*b);
+}
 
-
-
+console.log(2*5);
 
 /************************************************************** Task 2 **************************************************************/
 //Age in Dog years
 //write a function that takes your age and returns it to you in dog years - they say that 1 human year is equal to seven dog years 
 
+let myAge = 22;
 
+function dogAge() {
+    let dogAge = myAge * 7;
+}
 
-
+console.log(myAge*7);
 
 /************************************************************** Task 3 **************************************************************/
 //Dog feeder 
